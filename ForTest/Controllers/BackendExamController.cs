99573772c698b3322ec8860acd6135d92e5b29a8
Willy <aa8511234@gmail.com>
2025-05-07 10:03:23 +0800

    first commit

---
+++ ForTest/Controllers/BackendExamController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForTest.Controllers
+{
+    public class BackendExamController : ControllerBase
+    {
+
+
+        // GET: BackendExamController/Details/5
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            return Ok();
+        }
+
+        // POST: BackendExamController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return Ok();
+            }
+        }
+
+        // POST: BackendExamController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return Ok();
+            }
+        }
+
+        // POST: BackendExamController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return Ok();
+            }
+        }
+    }
+}
---
+++ ForTest/Utility/SQLUtility.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ForTest.Utility
+{
+    public class SQLUtility
+    {
+        public bool QuerySP(Object data)
+        {
+            string connectionString = "Server=your_server;Database=your_db;User Id=your_user;Password=your_password;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("YourStoredProcedureName", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // 範例參數（請依照實際的 SP 參數名稱與型別修改）
+                    cmd.Parameters.AddWithValue("@Param1", 123);
+                    cmd.Parameters.AddWithValue("@Param2", "Test");
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return true;
+        }
+
+        public object QuerySPWithRes(Object data)
+        {
+            string connectionString = "Server=your_server;Database=your_db;User Id=your_user;Password=your_password;";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("YourStoredProcedureName", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@SearchId", 456);
+
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("Column1: " + reader["Column1"]);
+                            // 讀取其他欄位...
+                        }
+                    }
+                }
+            }
+            return new object();
+        }
+    }
+}
